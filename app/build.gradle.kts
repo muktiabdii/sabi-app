2f2e1a175b2a9c48db911e081651135b3027d54a
muktiabdii <abdisyukur10@gmail.com>
2025-03-03 22:03:03 +0700

    Setup firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services") version "4.4.2"
 }
 
 android {
@@ -56,4 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Setup Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
 }
