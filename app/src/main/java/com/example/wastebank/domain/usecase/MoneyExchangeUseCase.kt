0cb5bd119e9c548ebec04beae2320ab982151684
muktiabdii <abdisyukur10@gmail.com>
2025-03-14 16:44:46 +0700

    Validasi input pada money exchange use case

--- app/src/main/java/com/example/wastebank/domain/usecase/MoneyExchangeUseCase.kt
+++ app/src/main/java/com/example/wastebank/domain/usecase/MoneyExchangeUseCase.kt
@@ -4,6 +4,13 @@ import com.example.wastebank.domain.repository.MoneyExchangeRepository
 
 class MoneyExchangeUseCase(private val moneyExchangeRepository: MoneyExchangeRepository) {
     fun exchangeMoney(point: Int, bankName: String, accountNumber: String, onResult: (Boolean, String?) -> Unit) {
+        
+        // Validasi input
+        if (point <= 0 || bankName.isBlank() || accountNumber.isBlank()) {
+            onResult(false, "Harap isi semua data")
+            return
+        }
+
         moneyExchangeRepository.exchangeMoney(point, bankName, accountNumber, onResult)
     }
 }
